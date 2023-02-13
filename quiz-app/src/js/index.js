5d89897c3dd39f6a48409c45f0a7f896dc24f382
Zalmai <shahzalmai98@gmail.com>
2023-02-13 01:52:36 -0800

    first_commit

---
+++ quiz-app/src/js/index.js
@@ -0,0 +1,399 @@
+
+const javaScriptQuizData =[
+  
+  {
+    question:'What is Diffrent between java and JavaScript?',
+    a:"Both are same",
+    b:"Java is part of JavaScript",
+    c:"Both are Diffrent languages",
+    d:"i don't know",
+    correct: 'd',
+
+  },
+  {
+    question:'When interpreter encounters an empty statements, what it will do:?',
+    a:"Shows a warning",
+    b:"Prompts to complete the statement",
+    c:"Throws an error",
+    d:"Ignores the statements",
+    correct: 'd',
+
+  },
+  {
+    question:'The "function" and " var" are known as?',
+    a:"Keywords",
+    b:"Data types",
+    c:"Declaration statements",
+    d:"Prototypes",
+    correct: 'c',
+
+  },
+  {
+    question:'Which of the following variables takes precedence over the others if the names are the same?',
+    a:"Global variable",
+    b:"The local element",
+    c:"The two of the above",
+    d:"None of the above",
+    correct: 'b',
+
+  },
+  {
+    question:'Which one of the following is the correct way for calling the JavaScript code?',
+    a:"Preprocessor",
+    b:"Triggering Event",
+    c:"RMI",
+    d:"Function/Method",
+    correct: 'd',
+
+  },
+  {
+    question:'Which of the following type of a variable is volatile?',
+    a:"Mutable variable",
+    b:"Dynamic variable",
+    c:"Volatile variable",
+    d:"Immutable variable",
+    correct: 'a',
+
+  },
+  {
+    question:'In the JavaScript, which one of the following is not considered as an error?',
+    a:"Syntax error",
+    b:"Missing of semicolons",
+    c:"Division by zero",
+    d:"Missing of Bracket",
+    correct: 'c',
+
+  },
+  {
+    question:'Which of the following number object function returns the value of the number?',
+    a:"toString()",
+    b:"valueOf()",
+    c:"toLocaleString()",
+    d:"toPrecision()",
+    correct: 'b',
+
+  },
+  {
+    question:'In JavaScript, what will be used for calling the function definition expression?',
+    a:"Function prototype",
+    b:"Function literal",
+    c:"Function calling",
+    d:"Function declaration",
+    correct: 'b',
+
+  },
+  {
+    question:'Which of the following one is the property of the primary expression?',
+    a:"Contains only keywords",
+    b:"basic expressions containing all necessary functions",
+    c:"contains variable references alone",
+    d:"stand-alone expressions",
+    correct: 'd',
+
+  },
+  
+]
+const pHpQuizData =[
+  
+  {
+    question:'PHP stands for?',
+    a:"Hypertext Preprocessor",
+    b:"Pretext Hypertext Preprocessor",
+    c:"Personal Home Processor",
+    d:"None of the above",
+    correct: 'a',
+
+  },
+  {
+    question:'Who is known as the father of PHP ?',
+    a:"Drek Kolkevi",
+    b:"List Barely",
+    c:"Rasmus Lerdrof",
+    d:"None of the above",
+    correct: 'c',
+
+  },
+  {
+    question:'Variable name in PHP starts with?',
+    a:"! (Exclamation)",
+    b:"$ (Dollar)",
+    c:"& (Ampersand)",
+    d:"# (Hash)",
+    correct: 'b',
+
+  },
+  {
+    question:' Which of the following is the default file extension of PHP?',
+    a:".php",
+    b:".hphp",
+    c:".xml",
+    d:".html",
+    correct: 'a',
+
+  },
+  {
+    question:' Which of the following is not a variable scope in PH?',
+    a:"Extern",
+    b:"Triggering Event",
+    c:"RMI",
+    d:"Function/Method",
+    correct: 'd',
+
+  },
+  {
+    question:' Which of the following is the use of strlen() function in PHP',
+    a:"Mutable variable",
+    b:"The strlen() function returns the length of string",
+    c:"Volatile variable",
+    d:"Immutable variable",
+    correct: 'b',
+
+  },
+  {
+    question:'In the JavaScript, which one of the following is not considered as an error?',
+    a:"Syntax error",
+    b:"Missing of semicolons",
+    c:"Division by zero",
+    d:"Missing of Bracket",
+    correct: 'c',
+
+  },
+  {
+    question:'What does PEAR stands for?',
+    a:"PHP extension and application repository",
+    b:"PHP enhancement and application reduce",
+    c:"PHP event and application repository",
+    d:"None of the above",
+    correct: 'a',
+
+  },
+  {
+    question:'Which of the following PHP function is used to generate unique id?',
+    a:"Function prototype",
+    b:"Function literal",
+    c:"uniqueid()",
+    d:"Function declaration",
+    correct: 'c',
+
+  },
+  {
+    question:'What is the use of fopen() function in PHP?',
+    a:"Contains only keywords",
+    b:"basic expressions containing all necessary functions",
+    c:"The fopen() function is used to open files in PHP",
+    d:"stand-alone expressions",
+    correct: 'c',
+
+  },
+  
+]
+
+const cpQuizData =[
+  
+  {
+    question:'The programming language that has the ability to create new data types is called?',
+    a:"Overloaded",
+    b:"Encapsulated",
+    c:"Reprehensible",
+    d:"Extensible",
+    correct: 'd',
+
+  },
+  {
+    question:'Which of the following is the original creator of the C++ language?',
+    a:"Drek Kolkevi",
+    b:"List Barely",
+    c:"Bjarne Stroustrup",
+    d:"None of the above",
+    correct: 'c',
+
+  },
+  {
+    question:' The C++ language is ______ object-oriented language?',
+    a:"! (Exclamation)",
+    b:"$ (Dollar)",
+    c:"Semi Object-oriented or Partial Object-oriented",
+    d:"# (Hash)",
+    correct: 'c',
+
+  },
+  {
+    question:'C++ is a ___ type of language?',
+    a:"High-level Language",
+    b:"Low-level language",
+    c:"Middle-level language",
+    d:".html",
+    correct: 'c',
+
+  },
+  {
+    question:' Which of the following is not a variable scope in PH?',
+    a:"Extern",
+    b:"Triggering Event",
+    c:"RMI",
+    d:"Function/Method",
+    correct: 'd',
+
+  },
+  {
+    question:'Which of the following refers to characteristics of an array',
+    a:"An array is a set of similar data items",
+    b:"An array is a set of distinct data items",
+    c:"An array can hold different types of datatypes",
+    d:"None of the above",
+    correct: 'a',
+
+  },
+  {
+    question:'Which of the following gives the 4th element of the array',
+    a:"Syntax error",
+    b:"Missing of semicolons",
+    c:"Array[3]",
+    d:"Missing of Bracket",
+    correct: 'c',
+
+  },
+  {
+    question:'Which type of memory is used by an Array in C++ programming language?',
+    a:"Contiguous",
+    b:"PHP enhancement and application reduce",
+    c:"PHP event and application repository",
+    d:"None of the above",
+    correct: 'a',
+
+  },
+  {
+    question:'How many types of the array are there in the C++ programming language?',
+    a:"Function prototype",
+    b:"Function literal",
+    c:"In the C++ programming language, there are two types of arrays",
+    d:"Function declaration",
+    correct: 'c',
+
+  },
+  {
+    question:'In C++, for what purpose the "rank()" is used?',
+    a:"Contains only keywords",
+    b:"basic expressions containing all necessary functions",
+    c:"It returns the dimension of the specified array",
+    d:"stand-alone expressions",
+    correct: 'c',
+
+  },
+  
+]
+let currentQuiz =0
+let score = 0
+
+
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+
+function loadQuiz(targetTopic){
+
+  const currentQuizData = targetTopic[currentQuiz]
+  h2.innerHTML=currentQuizData.question
+  a_text.innerHTML = currentQuizData.a
+  b_text.innerHTML = currentQuizData.b
+  c_text.innerHTML = currentQuizData.c
+  d_text.innerHTML = currentQuizData.d
+}
+
+
+
+const h1 = document.getElementById('headerr')
+const h2 = document.getElementById('question')
+const next = document.getElementById('next')
+const previous = document.getElementById('Previous')
+const cp = document.getElementById('cp')
+const php = document.getElementById('php')
+const  jasascript = document.getElementById('JavaScript')
+const qs = document.querySelectorAll('.qs')
+const quiz = document.getElementById('quiz')
+
+console.log(qs)
+previous.classList.add('hide')
+
+function onClickButton(){
+  cp.classList.add('hide')
+  h1.classList.add('hide')
+  php.classList.add('hide')
+  jasascript.classList.add('hide')
+
+  previous.style.display='block'
+  next.style.display='block'
+
+
+  for(let i in qs){
+    qs[i].style.display='block'
+  }
+
+}
+
+
+
+jasascript.addEventListener("click",()=>{
+ 
+  loadQuiz(javaScriptQuizData)
+  onClickButton()
+})
+
+php.addEventListener('click',()=>{
+
+  loadQuiz(pHpQuizData)
+  onClickButton()
+
+})
+
+cp.addEventListener('click',()=>{
+
+  loadQuiz(cpQuizData)
+  onClickButton()
+
+})
+
+
+
+//console.log(quizData[0].correct)
+
+function deselectAnswers() {
+  qs.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+  let answer
+  qs.forEach(answerEl => {
+      if(answerEl.checked) {
+          answer = answerEl.id
+      }
+  })
+  return answer
+}
+
+
+next.addEventListener('click',(evt)=>{
+  
+  const answer = getSelected()
+  if(answer) {
+     if(answer === javaScriptQuizData[currentQuiz].correct ||answer === pHpQuizData[currentQuiz].correct ||answer === cpQuizData[currentQuiz].correct ) {
+         score++
+         deselectAnswers()
+     }
+     currentQuiz++
+     if(currentQuiz < javaScriptQuizData.length ||currentQuiz < pHpQuizData.length ||currentQuiz < cpQuizData.length) {
+         loadQuiz(javaScriptQuizData)
+         loadQuiz(pHpQuizData)
+         loadQuiz(cpQuizData)
+         deselectAnswers()
+     } else {
+         quiz.innerHTML = `
+         <h2>You answered ${score}/${javaScriptQuizData.length} questions correctly</h2>
+         <button onclick="location.reload()">Reload</button>
+         `
+     }
+  }
+
+ 
+})
+
